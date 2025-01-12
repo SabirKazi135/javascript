2b727b7392136e43f3bf6cb717b8c775afc234b5
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-12 20:10:26 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-a = 5;
-b = 6;
-c = a + b;
-console.log(c);
+console.time("Execution Time");
+
+const names = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Eva",
+  "Frank",
+  "Grace",
+  "Hannah",
+  "Ian",
+  "Jack",
+  "Katie",
+  "Liam",
+  "Mia",
+  "Noah",
+  "Olivia",
+  "Paul",
+  "Quinn",
+  "Rachel",
+  "Sam",
+  "Tina",
+  "Uma",
+  "Victor",
+  "Wendy",
+  "Xavier",
+  "Yara",
+  "Zoe",
+  "Aaron",
+  "Bella",
+  "Carl",
+  "Diana",
+  "Ethan",
+  "Fiona",
+  "George",
+  "Hazel",
+  "Isaac",
+  "Julia",
+  "Kevin",
+  "Laura",
+  "Mason",
+  "Nina",
+];
+let count = 0;
+let l = names.length;
+for (let i = 0; i < names.length; i++) {
+  count + i;
+}
+console.timeEnd("Execution Time");
