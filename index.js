67097cc237033d080714d03707d09c86a3a81464
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-12 19:32:51 +0530

    commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello Sabir");
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const per1 = new Person("sabir", 22);
+const per2 = new Person("raees", 21);
+
+console.log(per1.name);
+console.log(per1.age);
+console.log(per2.name);
+console.log(per2.age);
