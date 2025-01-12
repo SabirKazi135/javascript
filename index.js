bab2784b5b126a7dfeacc7864f221c8899beeadc
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-12 19:35:46 +0530

    commit

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ class Person {
     this.name = name;
     this.age = age;
   }
+  both() {
+    return `Name is ${this.name} and Age is ${this.age}`;
+  }
 }
 
 const per1 = new Person("sabir", 22);
 const per2 = new Person("raees", 21);
 
-console.log(per1.name);
-console.log(per1.age);
-console.log(per2.name);
-console.log(per2.age);
+console.log(per1.both());
+console.log(per2.both());
