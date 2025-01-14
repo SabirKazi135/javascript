2f20abfc04e978caecd9261cf454b3a482961a87
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-14 22:22:30 +0530

    commit

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-function walkDog() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("You walk the dog");
-    }, 1500);
-  });
-}
+let a = 1;
+let b = 2;
 
-function cleanKitchen(callback) {
-  setTimeout(() => {
-    console.log("You clean the kitchen");
-    callback();
-  }, 2500);
-}
-function takeOutTrash(callback) {
-  setTimeout(() => {
-    console.log("You take out trash");
-    callback();
-  }, 500);
-}
-
-walkDog(() => {
-  cleanKitchen(() => {
-    takeOutTrash(() => console.log("You finish all the chores"));
-  });
-});
+console.log("sych");
+console.log(a);
+console.log(b);
