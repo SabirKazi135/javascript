a4c1b96d844e4c24d976135cea3f9bd411e8d42c
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-14 21:19:20 +0530

    commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("task1");
-console.log("task2");
-console.log("task3");
+function fun1(callback) {
+  setTimeout(() => {
+    console.log("task0");
+    callback();
+  }, 3000);
+}
+
+function fun2() {
+  console.log("task1");
+  console.log("task2");
+  console.log("task3");
+}
+
+fun1(fun2);
