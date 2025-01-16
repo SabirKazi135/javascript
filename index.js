204b5121a12ee7a9f0685fee7803ae1937709e79
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-16 12:52:42 +0530

    commit

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-const Person1 = {
-  firstName: "Sabir",
-  lastName: "Kazi",
-  age: 21,
-  isEmployee: false,
-  fullName: function () {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
-const Person2 = {
-  firstName: "Raees",
-  lastName: "jamadar",
-  age: 22,
-  isEmployee: false,
-  fullName: function () {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "1984", author: "George Orwell", year: 1949 },
+];
 
-console.log(Person1.firstName);
-console.log(Person1.lastName);
-console.log(Person1.age);
-console.log(Person1.isEmployee);
-console.log(Person1.fullName());
-
-console.log("---------------");
-console.log(Person2.firstName);
-console.log(Person2.lastName);
-console.log(Person2.age);
-console.log(Person2.isEmployee);
-console.log(Person2.fullName());
+// Printing details
+books.forEach((book) => {
+  console.log(`${book.title} by ${book.author}, published in ${book.year}`);
+});
