70564409a9f60769493b726c7280eca254d391f5
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-16 12:46:09 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const Person = {
+const Person1 = {
   firstName: "Sabir",
   lastName: "Kazi",
   age: 21,
   isEmployee: false,
+  fullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+const Person2 = {
+  firstName: "Raees",
+  lastName: "jamadar",
+  age: 22,
+  isEmployee: false,
+  fullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
 };
 
-console.log(Person.firstName);
-console.log(Person.lastName);
-console.log(Person.age);
-console.log(Person.isEmployee);
+console.log(Person1.firstName);
+console.log(Person1.lastName);
+console.log(Person1.age);
+console.log(Person1.isEmployee);
+console.log(Person1.fullName);
