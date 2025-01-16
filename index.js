950a3608191df9237aefc669e62629ddd55bf8f5
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-16 13:04:38 +0530

    commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const books = [
-  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-  { title: "1984", author: "George Orwell", year: 1949 },
-];
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 
-// Printing details
-books.forEach((book) => {
-  console.log(`${book.title} by ${book.author}, published in ${book.year}`);
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const per1 = new Person("Sabir", "Kazi", 21);
+const per2 = new Person("Raees", "Jamadar", 20);
+const per3 = new Person("Siddiq", "Untwale", 22);
+const per4 = new Person("Taif", "Bepari", 23);
+
+per1.forEach((per) => {
+  console.log(per.firstName);
+  console.log(per.lastName);
+  console.log(per.age);
+  console.log(per.fullName());
 });
