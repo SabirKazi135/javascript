c8a254d96770899077bb1201a70a67ed11b64f1f
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-17 21:08:31 +0530

    commit

--- index.js
+++ index.js
@@ -12,14 +12,23 @@ class animal {
 
 class Rabbit extends animal {
   name = "rabbit";
+  run() {
+    console.log(`This ${this.name} is running`);
+  }
 }
 
 class Fish extends animal {
   name = "fish";
+  swim() {
+    console.log(`This ${this.name} swemming`);
+  }
 }
 
 class Hawk extends animal {
   name = "hawk";
+  fly() {
+    console.log(`This ${this.name} fliying`);
+  }
 }
 
 const rabbit = new Rabbit();
@@ -28,3 +37,5 @@ const hawk = new Hawk();
 
 rabbit.alive = false;
 console.log(rabbit.alive);
+
+rabbit.run();
