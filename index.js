d4553d50e0cef62226179385da224c07ed63cf6e
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-18 00:18:36 +0530

    commit

--- index.js
+++ index.js
@@ -18,8 +18,21 @@ class Rectangle {
       console.log("Height Must be a positive number");
     }
   }
+
+  get width() {
+    return `${this._width.toFixed(1)}`;
+  }
+  get height() {
+    return `${this._Height.toFixed(1)}`;
+  }
+
+  get area() {
+    return `${(this._width * this._Height).toFixed(1)}cm^2`;
+  }
 }
 
 const rectangle = new Rectangle(3, 3);
 console.log(rectangle.height);
 console.log(rectangle.width);
+
+console.log(rectangle.area);
