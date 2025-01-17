28d9969fba468cd58a5a2cb68c09ec8ae0cf17bb
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-18 00:10:41 +0530

    commit

--- index.js
+++ index.js
@@ -1,45 +1,25 @@
-class Animal {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
   }
 
-  move(speed) {
-    console.log(`This ${this.name} moves at a speed of ${speed}kmp`);
+  set width(newWidth) {
+    if (newWidth > 0) {
+      this._width = newWidth;
+    } else {
+      console.log("Width Must be a positive number");
+    }
   }
-}
-
-class Rabbit extends Animal {
-  constructor(name, age, runSpeed) {
-    super(name, age); // Pass name and age to the parent constructor
-    this.runSpeed = runSpeed; // Set the Rabbit-specific property
-  }
-
-  run() {
-    console.log(`This ${this.name} can run`);
-    super.move(this.runSpeed);
-  }
-}
-
-class Fish extends Animal {
-  constructor(name, age, swimSpeed) {
-    super(name, age); // Pass name and age to the parent constructor
-    this.swimSpeed = swimSpeed; // Set the Fish-specific property
+  set height(newHeight) {
+    if (newHeight > 0) {
+      this._Height = newHeight;
+    } else {
+      console.log("Height Must be a positive number");
+    }
   }
 }
 
-class Hawk extends Animal {
-  constructor(name, age, flySpeed) {
-    super(name, age); // Pass name and age to the parent constructor
-    this.flySpeed = flySpeed; // Set the Hawk-specific property
-  }
-}
-
-// Creating instances
-const rabbit1 = new Rabbit("Rabo", 1, 25);
-const fish1 = new Fish("Fisho", 1, 15);
-const hawk1 = new Hawk("Hawko", 1, 40);
-
-// Logging the insta// Hawk { name: 'Hawko', age: 1, flySpeed: 40 }
-
-rabbit1.run();
+const rectangle = new Rectangle(-1000000, "pizza");
+console.log(rectangle.height);
+console.log(rectangle.width);
