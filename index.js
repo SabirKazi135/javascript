f2e0eff9b2280d9490801726963a23a5c26ace03
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-17 21:20:46 +0530

    commit

--- index.js
+++ index.js
@@ -6,24 +6,32 @@ class Animal {
 }
 
 class Rabbit extends Animal {
-  constructor(runSpeed) {
-    super();
-    this.runSpeed = runSpeed;
-  }
-}
-class Fish extends Animal {
-  constructor(swimSpeed) {
-    super();
-    this.swimSpeed = swimSpeed;
-  }
-}
-class Hawk extends Animal {
-  constructor(flySpeed) {
-    super();
-    this.flySpeed = flySpeed;
+  constructor(name, age, runSpeed) {
+    super(name, age); // Pass name and age to the parent constructor
+    this.runSpeed = runSpeed; // Set the Rabbit-specific property
   }
 }
 
-const rabbit1 = new Rabbit("rabo", 1, 25);
-const fish1 = new Fish("fisho", 1, 25);
-const hawk1 = new ("hawko", 1, 25)();
+class Fish extends Animal {
+  constructor(name, age, swimSpeed) {
+    super(name, age); // Pass name and age to the parent constructor
+    this.swimSpeed = swimSpeed; // Set the Fish-specific property
+  }
+}
+
+class Hawk extends Animal {
+  constructor(name, age, flySpeed) {
+    super(name, age); // Pass name and age to the parent constructor
+    this.flySpeed = flySpeed; // Set the Hawk-specific property
+  }
+}
+
+// Creating instances
+const rabbit1 = new Rabbit("Rabo", 1, 25);
+const fish1 = new Fish("Fisho", 1, 15);
+const hawk1 = new Hawk("Hawko", 1, 40);
+
+// Logging the instances
+console.log(rabbit1); // Rabbit { name: 'Rabo', age: 1, runSpeed: 25 }
+console.log(fish1); // Fish { name: 'Fisho', age: 1, swimSpeed: 15 }
+console.log(hawk1); // Hawk { name: 'Hawko', age: 1, flySpeed: 40 }
