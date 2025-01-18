2d1262befa3f248e9e8e553f029720a840c1f20e
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-18 21:14:14 +0530

    commit

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-class Vehicle {
-  constructor(make, model, year) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  set width(newWidth) {
+    if (
+      typeof newWidth == "number" &&
+      newWidth > 0 &&
+      newWidth != NaN &&
+      newWidth != Infinity
+    ) {
+      this._width = newWidth;
+    } else {
+      console.error("Width must be a potive number");
+    }
+  }
+  set height(newHeight) {
+    if (
+      typeof newHeight == "number" &&
+      newHeight > 0 &&
+      newHeight != NaN &&
+      newHeight != Infinity
+    ) {
+      this._height = newHeight;
+    } else {
+      console.error("Height must be a potive number");
+    }
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  get area() {
+    return this._height * this.width;
   }
 }
 
-class Car extends Vehicle {
-  constructor(make, model, year, numberOfDoors) {
-    super(make, model, year);
-    this.numberOfDoors = numberOfDoors;
-  }
-  getDetail() {
-    return `Car : ${this.make} ${this.model}, Year: ${this.year}, Doors: ${this.numberOfDoors}`;
-  }
-}
-class Bike extends Vehicle {
-  constructor(make, model, year, type) {
-    super(make, model, year);
-    this.type = type;
-  }
-  getDetail() {
-    return `Bike : ${this.make} ${this.model}, Year: ${this.year}, Type: ${this.type}`;
-  }
-}
-
-const car1 = new Car("Toyota", "Camry", 2020, 4);
-const car2 = new Car("Honda", "Civic", 2018, 2);
-const car3 = new Car("Ford", "Mustang", 2021, 2);
-
-const bike1 = new Bike("Harley-Deavidson", "iron 883", 2019, "Cruiser");
-const bike2 = new Bike("Yamaha", "YZF R3", 2022, "Sport");
-const bike3 = new Bike("Kawasaki", "Ninja 400", 2021, "Sport");
-
-console.log(car1.getDetail());
-console.log(car2.getDetail());
-console.log(car3.getDetail());
-
-console.log(bike1.getDetail());
-console.log(bike2.getDetail());
-console.log(bike3.getDetail());
+const rect1 = new Rectangle(5, 10);
+console.log(rect1.width);
+console.log(rect1.height);
+console.log(rect1.area);
