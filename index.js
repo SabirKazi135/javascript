f9bf9045c9b536798eb559575343548c171bbcd6
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-18 16:48:26 +0530

    commit

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-  constructor(make, model, year, numberOfDoors, getDetails) {
+  constructor(make, model, year, numberOfDoors) {
     super(make, model, year);
     this.numberOfDoors = numberOfDoors;
-    this.getDetails = getDetails;
+  }
+  getDetail() {
+    return `Car : ${this.make} ${this.model}, Year: ${this.year}, Doors: ${this.numberOfDoors}`;
   }
 }
-class bike extends Vehicle {
+class Bike extends Vehicle {
   constructor(make, model, year, type) {
     super(make, model, year);
     this.type = type;
+  }
+  getDetail() {
+    return `Bike : ${this.make} ${this.model}, Year: ${this.year}, Type: ${this.type}`;
   }
 }
 
@@ -24,6 +29,14 @@ const car1 = new Car("Toyota", "Camry", 2020, 4);
 const car2 = new Car("Honda", "Civic", 2018, 2);
 const car3 = new Car("Ford", "Mustang", 2021, 2);
 
-const bike1 = new bike("Harley-Deavidson", "iron 883", 2019, "Cruiser");
-const bike2 = new bike("Yamaha", "YZF R3", 2022, "Sport");
-const bike3 = new bike("Kawasaki", "Ninja 400", 2021, "Sport");
+const bike1 = new Bike("Harley-Deavidson", "iron 883", 2019, "Cruiser");
+const bike2 = new Bike("Yamaha", "YZF R3", 2022, "Sport");
+const bike3 = new Bike("Kawasaki", "Ninja 400", 2021, "Sport");
+
+console.log(car1.getDetail());
+console.log(car2.getDetail());
+console.log(car3.getDetail());
+
+console.log(bike1.getDetail());
+console.log(bike2.getDetail());
+console.log(bike3.getDetail());
