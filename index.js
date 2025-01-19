dc986a02e3a8592fed809d4aad8c1f1027e59783
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-19 12:47:54 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const colors = ["red", "green", "blue", "black", "white"];
+const person1 = {
+  firstName: "Sabir",
+  lastName: "Kazi",
+  age: 21,
+  job: "nonjob",
+};
+const person2 = {
+  firstName: "Raees",
+  lastName: "Jamadar",
+  age: 20,
+  job: "job",
+};
 
-const [firstColor, secondColor, thirdColor, ...otherColors] = colors;
+const { firstName, lastName, age, job } = person1;
 
-console.log(firstColor);
-console.log(secondColor);
-console.log(thirdColor);
-console.log(otherColors);
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(job);
