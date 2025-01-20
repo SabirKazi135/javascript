f4388dde89a911c61d95794c469ec1fa7f19333e
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-20 15:38:44 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-
-function Shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const random = Math.floor(Math.random() * (i + 1));
-    [array[i], array[random]] = [array[random], array[i]];
-  }
+function increament() {
+  let count = 0;
+  count++;
+  console.log(`Count increased to ${count}`);
 }
 
-Shuffle(array);
+increament();
+increament();
+increament();
+increament();
+increament();
