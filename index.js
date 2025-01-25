8c817b3b1f2858f739447063a07312ce42e24c5b
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-25 16:44:02 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const dividend = window.prompt("Enter a diviend: ");
-const divisor = window.prompt("Enter a divisor: ");
+try {
+  const dividend = window.prompt("Enter a diviend: ");
+  const divisor = window.prompt("Enter a divisor: ");
 
-const result = dividend / divisor;
-console.log(result);
+  const result = dividend / divisor;
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
+
+console.log("you have rechaed end");
