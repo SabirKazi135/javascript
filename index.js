34679502178da7b184da40e027da8916911f5c1b
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-25 16:47:12 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 try {
-  const dividend = window.prompt("Enter a diviend: ");
+  const dividend = Number(window.prompt("Enter a diviend: "));
   const divisor = window.prompt("Enter a divisor: ");
-
+  if (divisor == 0) {
+    throw new Error("you cant divide my zero");
+  }
+  if (isNaN(dividend) || isNaN(divisor)) {
+    throw new Error("value must be an number");
+  }
   const result = dividend / divisor;
   console.log(result);
 } catch (error) {
